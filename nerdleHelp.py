629642b6381808f1669c8969befdfd6a178880b3
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-04-04 12:35:56 -0400

    create initial dictionary

--- nerdleHelp.py
+++ nerdleHelp.py
@@ -2,7 +2,16 @@
 # Date: April 4, 2022
 # Latest Revision: April 4, 2022
 
+def initDict():
+    bank = {
+        "nums": [0,1,2,3,4,5,6,7,8,9],
+        "symbols": ['+', '-', '*', '/', '='],
+    }
+
+    return bank
+
 def main():
+    initDict()
     print("Hello World!")
 
 main()
