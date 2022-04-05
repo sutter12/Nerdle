e1cee670603515e4d09f138c4e993af31afa4c62
Alex Sutter <46454849+sutter12@users.noreply.github.com>
2022-04-05 14:54:23 -0400

    getting input part 1

--- nerdleHelp.py
+++ nerdleHelp.py
@@ -29,12 +29,51 @@ def removeSymbol(symbol, bank):
     bank["symbols"] = symbols
     return bank
 
+def checkLength(guess):
+    if len(guess) == 8:
+        return True
+    else: 
+        print("invalid guess (incorrect length)!")
+        return False
+
+def checkEquals(guess):
+    symbols = ['+', '-', '*', '/']
+    hitEqual = False
+    for i in guess:
+        if i == '=':
+            hitEqual = True
+        if hitEqual:
+            if i in symbols:
+                print("invalid guess (operation after equals)!")
+                return False
+    if hitEqual:
+        return True
+    else:
+        return False
+
+def compute(check):
+    symbols = ['+', '-', '*', '/']
+    tempNum = 0
+    nums = []
+    for i in range(len(check)):
+        if i in symbols:
+            print("hi")
+
+
+
+def getGuess():
+    valid = False
+    while not valid:
+        guess = input("What is your guess")
+        lengthCheck = checkLength(guess)
+        equalSign = checkEquals(guess)
+        computes = compute(guess)
+            
+    return guess
+
 def main():
     bank = initDict()
     print(str(bank) + "\n")
-    bank = removeNum(5, bank)
-    print(str(bank) + "\n")
-    bank = removeSymbol('*', bank)
-    print(str(bank) + "\n")
+    print(getGuess())
 
 main()
